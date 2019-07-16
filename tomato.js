169189c8f02d5d4dff66bea06d70b4261af738b1
masak <oicwam.orz1228@gmail.com>
2019-07-16 23:43:34 +0900

    jQueryを導入。とりあえずタイマー処理からの通知まではできるようになった。

--- tomato.js
+++ tomato.js
@@ -1,27 +1,90 @@
-const workTime = document.getElementById('work-time');
-const breakTime = document.getElementById('break-time');
-const testNotification = document.getElementById('notification-test');
+$(() => {
 
-const workTimeCount = document.getElementById('work-time-count');
-const breakTimeCount = document.getElementById('break-time-count');
+    const $workTime = $('#work-time');
+    const $breakTime = $('#break-time');
 
-workTimeCount.innerText = `${workTime.value}:00`;
-breakTimeCount.innerText = `${breakTime.value}:00`;
+    const $workTimeCount = $('#work-time-count');
+    const $breakTimeCount = $('#break-time-count');
 
-testNotification.addEventListener('click', () => {
-    createNotification();
+    const $start = $('#count-down-start');
+
+    $workTimeCount.text(`${$workTime.val()}:00`);
+    $breakTimeCount.text(`${$breakTime.val()}:00`);
+
+    $workTime.on('change', () => {
+        $workTimeCount.text(`${$workTime.val()}:00`);
+    });
+    $breakTime.on('change', () => {
+        $breakTimeCount.text(`${$breakTime.val()}:00`);
+    });
+
+    $start.on('click', () => {
+
+        let min = Number($workTime.val());
+        let sec = 0;
+        // テスト用
+        // let min = 0;
+        // let sec = 5;
+        const intervalId = setInterval(() => {
+
+            if (isTimeUp(min, sec)) {
+                $workTimeCount.text('Time up!');
+                createNotification();
+                clearInterval(intervalId);
+                return;
+            }
+
+            if (hasPassedOneMinute(sec)) {
+                sec = 59;
+                min--;
+            } else {
+                sec--;
+            }
+
+            $workTimeCount.text(`${zeroPadding(min)}:${zeroPadding(sec)}`);
+
+        }, 1000);
+    });
+
+    function createNotification() {
+        chrome.notifications.create(
+            '',
+            {
+                type: 'basic',
+                title: '通知テストだよー',
+                iconUrl: 'img/tomato_128.png',
+                message: 'これは通知の確認用テストです。'
+            },
+            (notificationId) => {
+                console.log(`通知が表示されました。通知ID: ${notificationId}`);
+            });
+    }
+
+    /**
+     * 残り時間が無くなった
+     * @param {number} remainingTimeMinutes
+     * @param {number} remainingSecond
+     * @return {boolean}
+     */
+    function isTimeUp(remainingTimeMinutes, remainingSecond) {
+        return remainingTimeMinutes === 0 && remainingSecond === 0;
+    }
+
+    /**
+     * 1分が経過した
+     * @param second
+     * @return {boolean}
+     */
+    function hasPassedOneMinute(second) {
+        return second === 0;
+    }
+
+    /**
+     * 2桁までを0埋めする
+     * @param {number} num
+     * @return {string}
+     */
+    function zeroPadding(num) {
+        return String(num).padStart(2, '0');
+    }
 });
-
-function createNotification() {
-    chrome.notifications.create(
-        '',
-        {
-            type: 'basic',
-            title: '通知テストだよー',
-            iconUrl: 'img/tomato_128.png',
-            message: 'これは通知の確認用テストです。'
-        },
-        (notificationId) => {
-            console.log(`通知が表示されました。通知ID: ${notificationId}`);
-        });
-}
