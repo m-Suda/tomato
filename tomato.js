fdb0edc997f89b78b29169567bdbf9af78c6862f
Masaki Suda <ga3783ol@g.tdi.co.jp>
2019-07-17 16:19:58 +0900

    カウントダウンのstart, stop, restart, resetを実装

--- tomato.js
+++ tomato.js
@@ -1,52 +1,96 @@
 $(() => {
 
+    const EVERY_SECOND = 1000;
+
     const $workTime = $('#work-time');
     const $breakTime = $('#break-time');
 
     const $workTimeCount = $('#work-time-count');
     const $breakTimeCount = $('#break-time-count');
 
+    const $buttonArea = $('#button-area');
     const $start = $('#count-down-start');
+    const $restart = $('#count-down-restart');
+    const $stop = $('#count-down-stop');
+    const $reset = $('#timer-reset');
 
-    $workTimeCount.text(`${$workTime.val()}:00`);
-    $breakTimeCount.text(`${$breakTime.val()}:00`);
+    let TIMER = '';
 
     $workTime.on('change', () => {
         $workTimeCount.text(`${$workTime.val()}:00`);
     });
     $breakTime.on('change', () => {
-        $breakTimeCount.text(`${$breakTime.val()}:00`);
+        $breakTimeCount.text(`${zeroPadding($breakTime.val())}:00`);
     });
 
     $start.on('click', () => {
-
-        let min = Number($workTime.val());
-        let sec = 0;
-        // テスト用
-        // let min = 0;
-        // let sec = 5;
-        const intervalId = setInterval(() => {
-
-            if (isTimeUp(min, sec)) {
-                $workTimeCount.text('Time up!');
-                createNotification();
-                clearInterval(intervalId);
-                return;
-            }
-
-            if (hasPassedOneMinute(sec)) {
-                sec = 59;
-                min--;
-            } else {
-                sec--;
-            }
-
-            $workTimeCount.text(`${zeroPadding(min)}:${zeroPadding(sec)}`);
-
-        }, 1000);
+        initializeTime();
+        TIMER = setInterval(() => {
+            countDown();
+            viewTime();
+        }, EVERY_SECOND);
     });
 
-    const createNotification = () => {
+    $stop.on('click', () => {
+        clearInterval(TIMER);
+    });
+
+    $restart.on('click', () => {
+        TIMER = setInterval(() => {
+            countDown();
+            viewTime();
+        }, EVERY_SECOND);
+    });
+
+    $reset.on('click', () => {
+        initializeTime();
+        viewTime();
+    });
+
+    let min = 0;
+    let sec = 0;
+
+    /**
+     * 残り時間をリセットする。
+     */
+    const initializeTime = () => {
+        min = Number($workTime.val());
+        sec = 0;
+        // テスト用
+        // min = 0;
+        // sec = 5;
+    };
+
+    /**
+     * カウントダウンする。
+     */
+    const countDown = () => {
+        if (isTimeUp(min, sec)) {
+          $workTimeCount.text('Time up!');
+          showNotification();
+          clearInterval(intervalId);
+          return;
+        }
+
+        if (hasPassedOneMinute(sec)) {
+          sec = 59;
+          min--;
+        } else {
+          sec--;
+        }
+    };
+
+    /**
+     * 時間を表示する。
+     */
+    const viewTime = () => {
+      $workTimeCount.text(`${zeroPadding(min)}:${zeroPadding(sec)}`);
+    };
+
+    /**
+     * Chromeの通知を作成し、表示する。
+     */
+    const showNotification = () => {
         chrome.notifications.create(
             '',
             {
